812a611713e3483c8632601555339396a770bad8
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-08-13 11:18:20 +0700

    Update __init__.py

--- colpali_engine/models/internvl2/colinternvl2/internvl/patch/__init__.py
+++ colpali_engine/models/internvl2/colinternvl2/internvl/patch/__init__.py
@@ -4,18 +4,18 @@
 # Licensed under The MIT License [see LICENSE for details]
 # --------------------------------------------------------
 
-from .internlm2_packed_training_patch import replace_internlm2_attention_class
-from .llama2_flash_attn_monkey_patch import replace_llama2_attn_with_flash_attn
-from .llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
-from .llama_packed_training_patch import replace_llama_attention_class
+# from .internlm2_packed_training_patch import replace_internlm2_attention_class
+# from .llama2_flash_attn_monkey_patch import replace_llama2_attn_with_flash_attn
+# from .llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+# from .llama_packed_training_patch import replace_llama_attention_class
 from .llama_rmsnorm_monkey_patch import \
     replace_llama_rmsnorm_with_fused_rmsnorm
 from .pad_data_collator import concat_pad_data_collator, pad_data_collator
-from .phi3_packed_training_patch import replace_phi3_attention_class
-from .qwen2_packed_training_patch import replace_qwen2_attention_class
+# from .phi3_packed_training_patch import replace_phi3_attention_class
+# from .qwen2_packed_training_patch import replace_qwen2_attention_class
 from .train_dataloader_patch import replace_train_dataloader
 from .train_sampler_patch import replace_train_sampler
-from .internvl2_gradient_checkpointing import patch_internvl2_gradient_checkpointing
+# from .internvl2_gradient_checkpointing import patch_internvl2_gradient_checkpointing
 
 __all__ = ['replace_llama_attn_with_flash_attn',
            'patch_internvl2_gradient_checkpointing',
